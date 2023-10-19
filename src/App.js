4f395721853abe7bcb2b856fac7265d3854c2f41
Rekha Giri <rekhagirigiri84@gmail.com>
2023-10-19 22:54:42 +0530

    First Files Added

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="box">
+          <h1>Let's Get Started</h1>
+          <h2>Myself Rekha Giri</h2>
+          <h2>I am currently pursuing BSc. (hons) Computer Science from IPCW</h2>
+          <h2>Indraprastha College For Women - Delhi University</h2>
+        </div>
       </header>
     </div>
   );
---
+++ src/self.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function self() {
+  return (
+    <div>Heyy
+        <div>
+            <h1>Rekha</h1>
+        </div>
+    </div>
+  )
+}
+
+export default self
