d1573ac4bae0e50d9933f07fb4c15086ffa83c42
Rekha Giri <rekhagirigiri84@gmail.com>
2023-10-21 00:09:39 +0530

    First App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ImageGallery from './component/imageGallery';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div class="box">
-          <h1>Let's Get Started</h1>
-          <h2>Myself Rekha Giri</h2>
-          <h2>I am currently pursuing BSc. (hons) Computer Science from IPCW</h2>
-          <h2>Indraprastha College For Women - Delhi University</h2>
-        </div>
+
+        <ImageGallery/>
       </header>
     </div>
   );
